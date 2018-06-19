c658c65606adaed7228b1e6efd45c3924e0cf305
Aran <aran@500tech.com>
2018-06-19 16:15:55 +0300

    add package infrastructure

---
+++ src/index.development.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import ConnectedApp from './ConnectedApp';
+
+ReactDOM.render(<ConnectedApp />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const webpack = require('webpack');
+const path = require('path');
+
+// PLUGINS
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: './public/index.html',
+  filename: './index.html'
+});
+
+const config = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      }
+    ]
+  },
+  plugins: []
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.entry = './src/index.development.js';
+
+    config.plugins.push(htmlWebpackPlugin);
+
+    config.devServer = {
+      historyApiFallback: true
+    };
+  }
+
+  if (argv.mode === 'production') {
+    config.entry = './src/index.production.js';
+
+    config.output = {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'index.js',
+      libraryTarget: 'umd',
+      globalObject: 'this',
+      library: 'eagledashboard'
+    };
+
+    config.externals = {
+      'react': {
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'react',
+        root: '_'
+      },
+      'react-dom': {
+        commonjs: 'react-dom',
+        commonjs2: 'react-dom',
+        amd: 'react-dom',
+        root: '_'
+      }
+    };
+  }
+
+  return config;
+};
