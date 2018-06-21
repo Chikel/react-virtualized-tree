2b03025858212ce197d060a33efc6984a08e693b
Aran <aran@500tech.com>
2018-06-21 18:26:59 +0300

    add animation (opacity) functionality

--- src/Tree.tsx
+++ src/Tree.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {AutoSizer, List} from 'react-virtualized';
-import {flatten, update} from 'lodash/fp';
+import {flatten, update, get, some} from 'lodash/fp';
 
 interface Item {
   id: string | number;
@@ -17,12 +17,16 @@ interface TreeProps {
 interface TreeState {
   expandedMap: { [treeLevel: number]: { [itemId: number]: boolean } };
   normalizedTreeItems: Item[];
+  unmountingChildrenParentId: boolean;
 }
 
 class Tree extends React.Component<TreeProps, TreeState> {
+  list: List;
+
   state = {
     expandedMap: {},
-    normalizedTreeItems: []
+    normalizedTreeItems: [],
+    unmountingChildrenParentId: null
   };
 
   componentDidMount() {
@@ -36,7 +40,7 @@ class Tree extends React.Component<TreeProps, TreeState> {
     }
 
     return flatten(
-      nodes.map(node => node.children && expandedMap && expandedMap[level][node.id] ?
+      nodes.map(node => node.children && get([level, node.id], expandedMap) ?
         [{...node, level}, ...flatten(this.getNormalizedTreeItems(node.children, expandedMap, level + 1))] :
         [{...node, level}])
     )
@@ -44,24 +48,47 @@ class Tree extends React.Component<TreeProps, TreeState> {
 
   renderRow = ({key, index, style}) => {
     const {itemRenderer} = this.props;
-    const {normalizedTreeItems} = this.state;
+    const {normalizedTreeItems, unmountingChildrenParentId} = this.state;
     const currentItem = normalizedTreeItems[index];
+    const parentToMinimize = normalizedTreeItems[unmountingChildrenParentId];
+    const isRowUnmounting = parentToMinimize && some(c => c.id === currentItem.id, parentToMinimize.children);
 
     return (
-      <div key={currentItem.id} style={style}>
+      <div key={currentItem.id}
+           style={{...style, opacity: 0, transition: `opacity 0.5s`}}
+           ref={node => node && this.handleRowAnimation(node, isRowUnmounting)}>
         {itemRenderer(currentItem, () => this.handleRowClicked(currentItem.level, currentItem.id))}
       </div>
     );
   };
 
+  handleRowAnimation = (node, isRowUnmounting) => {
+    setTimeout(() => {
+      node.style.opacity = isRowUnmounting ? '0' : '1'
+    }, 0)
+  };
+
   handleRowClicked = (rowLevel, rowId) => {
     const {items} = this.props;
-    const expandedMap = update([rowLevel, rowId], bool => !bool, this.state.expandedMap);
+    const {expandedMap} = this.state;
+    const unmountingChildrenParentId = get([rowLevel, rowId], expandedMap) ? rowId : null;
+    const updatedExpandedMap = update([rowLevel, rowId], bool => !bool, expandedMap);
 
-    this.setState({
-      expandedMap,
-      normalizedTreeItems: this.getNormalizedTreeItems(items, expandedMap)
-    });
+    this.setState(
+      {unmountingChildrenParentId},
+      () => {
+
+        if (unmountingChildrenParentId) {
+          this.list.forceUpdateGrid();
+        }
+
+        setTimeout(() => {
+          this.setState({
+            expandedMap: updatedExpandedMap,
+            normalizedTreeItems: this.getNormalizedTreeItems(items, updatedExpandedMap),
+          })
+        }, 500)
+      });
   };
 
   render() {
@@ -73,6 +100,7 @@ class Tree extends React.Component<TreeProps, TreeState> {
         <AutoSizer>
           {({width, height}) => (
             <List
+              ref={node => this.list = node}
               width={width}
               height={height}
               rowCount={normalizedTreeItems.length}
